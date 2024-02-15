601edebe2be5394c98f67eaa1654261237e47a19
Navaneesiva <navaneethan.cs21@bitsathy.ac.in>
2024-02-15 09:31:17 +0530

    first commit

---
+++ server.cjs
@@ -0,0 +1,95 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+// Importing required functions from dbConnection.cjs
+const {connectToDb, getDb} = require('./dbconnection.cjs')
+const {ObjectId} =require('mongodb')
+const cors =require('cors')
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+let db
+connectToDb(function(error) {
+    if(error) {
+        console.log('Could not establish connection...')
+        console.log(error)
+    } else { // if no error in establishing connection
+       const port = process.env.PORT|| 7000
+       app.listen(port)
+        db = getDb()
+        console.log('Listening on port 7000...')
+    }
+})
+
+
+
+app.post('/add-user', function(request, response) {
+    db.collection('connected').insertMany(request.body).then(function() {
+        response.status(201).json({
+            "status" : "Entry added successfully"
+        })
+    }).catch(function () {
+        response.status(500).json({
+             "status" : "Entry not added"
+        })
+    })
+})
+
+
+app.get('/get-entries', function(request, response) {
+    // Declaring an empty array
+    const entries = []
+    db.collection('connected')
+    .find()
+    .forEach(entry => entries.push(entry))
+    .then(function() {
+        response.status(200).json(entries)
+    }).catch(function() {
+        response.status(500).json({
+            "status" : "Could not fetch documents"
+        })
+    })
+})
+
+app.delete('/delete-entry', function(request, response) {
+    if(ObjectId.isValid(request.query.id)) {
+        db.collection('connected').deleteOne({
+            _id : new ObjectId(request.query.id)
+        }).then(function() {
+            response.status(200).json({
+                "status" : "Entry successfully deleted"
+            })
+        }).catch(function() {
+            response.status(500).json({
+                "status" : "Entry not deleted"
+            })
+        })
+    } else {
+        response.status(500).json({
+            "status" : "ObjectId not valid"
+        })
+    }
+})
+
+app.patch('/update-entry/:id', function(request, response) {
+    if(ObjectId.isValid(request.params.id)) {
+        db.collection('connected').updateMany(
+            { _id : new ObjectId(request.params.id) }, // identifier : selecting the document which we are going to update
+            { $set : request.body } // The data to be updated
+        ).then(function() {
+            response.status(200).json({
+                "status" : "Entry updated successfully"
+            })
+        }).catch(function() {
+            response.status(500).json({
+                "status" : "Unsuccessful on updating the entry"
+            })
+        })
+    } else {
+        response.status(500).json({
+            "status" : "ObjectId not valid"
+        })
+    }
+})
+
